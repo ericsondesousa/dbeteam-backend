7710840a2089e085d5b29eeccecda414f126949c
Éricson Sousa <ericson@e-flips.com.br>
2023-05-26 17:20:12 -0300

    Carbon locale

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -21,5 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+        Carbon::setLocale($this->app->getLocale());
     }
 }
